56bf28279d663e9a8f5a7e3ca911683c518dc121
jana <janamurugesan01@gmail.com>
2021-11-15 19:17:59 +0530

    intdouble basics

--- 4.intdouble.dart
+++ 4.intdouble.dart
@@ -1,6 +1,17 @@
+import 'dart:math';
+
 void main(List<String> args) {
   int number = 10;
   double d = 15.0;
-  print(number);
+  number += 5;
+  print(number); //15
+  number++;
+  print(number); // Increment 16
   print(d);
+
+  //! Min function (Math library)
+  print(min(5, 9)); // 5
+
+  //! Square root
+  print(sqrt(64)); // 8.0
 }
